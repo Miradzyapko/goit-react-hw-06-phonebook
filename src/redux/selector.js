958e63b401f2571b84d3e734e14e0abdb608c150
Miradzyapko <mira.dzyapko@gmail.com>
2023-07-25 02:14:13 +0300

    fix bug

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,6 +1,6 @@
 import { createSelector } from "@reduxjs/toolkit";
 
-export const selectContacts = state => state.contacts;
+export const selectContacts = state => state.contacts.contacts;
 /*export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;*/
 export const selectFilter = state => state.filter;
@@ -9,7 +9,7 @@ export const selectVisibleContacts = createSelector(
  [selectContacts, selectFilter], 
  (contacts, queryFilter) => {
     const filterContacts = queryFilter
-  ?  contacts.filter(contact => 
+  ?  contacts.contacts.filter(contact => 
          contact.name.toLowerCase().includes(queryFilter)
     )
     : contacts;
